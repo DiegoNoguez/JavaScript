51383467293f47004430a081528dac1083529efd
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-08 09:07:11 -0600

    Se intengran cambios menores

--- basico/tiposDatos.js
+++ basico/tiposDatos.js
@@ -1,2 +1,22 @@
 //En este archivo se veran los tipos de dstos presntes en JS
-//Empezando con Number que repreenta a los datos numericos
+//Empezando con Number que repreenta a los datos numericos  
+//EL operador + es usado para reperesentar suma en JS
+const suma=10+20;
+console.log(suma);
+//Operador para represntar la resta dentro de JS es -
+const resta=10-5;
+
+//operador de multiplicaión *
+const multiplicaion=10*5;
+
+//operador de divison /
+const division=50/5;
+
+//forma de adición de un numero en 1  operador que es usado ++
+//ejemplo var i=i+1; o 
+let x=5;
+x++;
+//para disminuir es lo mismo pero con 
+x--;
+
+let numeroDecimal=5.7;
