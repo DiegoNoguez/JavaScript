53d3a50b16696d47c1d086fb0a3dac485e355faa
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-11 07:02:30 -0600

    Strings

--- basico/manejoCadenas.js
+++ basico/manejoCadenas.js
@@ -1,4 +1,4 @@
 //En este arcivo se mostrara el manejo de cadenas dentro de cadenas en un string
 
 const cadenaDentro= "Arturo, \"Esta aprendidendo JS\".";
-console.log(cadenaDentro);
+console.log(cadenaDentro); //impresion en terminal de la cadana(string)
