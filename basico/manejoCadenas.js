d6c104c9c6d4b984cbfa13c468b1c796a3568706
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-13 19:52:54 -0600

    Actualizaciones menores

--- basico/manejoCadenas.js
+++ basico/manejoCadenas.js
@@ -4,3 +4,5 @@ const cadenaDentro= "Arturo, \"Esta aprendidendo JS\".";
 console.log(cadenaDentro); //impresion en terminal de la cadana(string)
 
 //Asi mismo se pueden hacer distintas operaciones con los Strings 
+//concadenacion pero solo usando comillas simples y dobles.
+const myStr = "<a href='http://www.example.com' target='_blank'>Link</a>";
