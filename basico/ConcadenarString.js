70cce7114b285612122ff5ec126590e53320ad66
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-13 20:18:50 -0600

    Actualizacion menor y se agrega nuevo archivo

---
+++ basico/ConcadenarString.js
@@ -0,0 +1,15 @@
+// En este código se muestre como se concadenan los string con el operador de +
+const myStr = "This is the start "+"This is the end"; 
+
+//otra forma de concadenar string en js es con el operador +=
+let ejemplo = "This is the first sentence. ";
+ejemplo+= "This is the second sentence.";
+
+//Construccion de cadenas con variables(string)
+const myName = "DiegoNoguez";
+const ejemplo2 = "My name is "+myName+" and I am well!";
+
+//Agregacion de variables a cadenas(strings) con el operador +=
+const someAdjective = "Es asombroso ";
+let eje1 = "Learning to code is ";
+myStr+=someAdjective;
--- basico/manejoCadenas.js
+++ basico/manejoCadenas.js
@@ -5,4 +5,4 @@ console.log(cadenaDentro); //impresion en terminal de la cadana(string)
 
 //Asi mismo se pueden hacer distintas operaciones con los Strings 
 //concadenacion pero solo usando comillas simples y dobles.
-const myStr = "<a href='http://www.example.com' target='_blank'>Link</a>";
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
