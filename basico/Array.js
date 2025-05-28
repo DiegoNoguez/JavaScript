14e15514f7bd57ce272eeb077a8c3aa2c965ca25
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-27 18:20:06 -0600

    Manejo de Array

---
+++ basico/Array.js
@@ -0,0 +1,7 @@
+//Uso de Arrays y creación de los mismos dentro de JS
+//Defincion de un array
+const Array =["Hola", 5, "Array"];
+   
+
+//Anidar un array dentro de otro
+array=["Hola", 5, "Array", [1, 2, 3]];
