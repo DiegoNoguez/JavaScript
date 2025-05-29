b40fa0f48d3c50756d36f06ddddf714323249080
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-28 18:58:13 -0600

    Mas formas de manejar arrays en JS

--- basico/Array.js
+++ basico/Array.js
@@ -12,3 +12,19 @@ const data = Array[2]; // "Array"
 
 //modificar un array con indices 
 Array[1] =15;
+
+
+//Aceder a arrays multidimensionales mediante indices
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 88, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+console.log(myData);    // 88
+
+//Manipulación de arrays mediante el metodo push
+Array.push("Nuevo Elemneto");//se añade aelemento al final del propio array 
+
