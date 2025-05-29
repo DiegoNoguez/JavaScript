2ef6f76c23cf1a518c71bfe6351a144c7cc61a0d
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-05-28 18:29:07 -0600

    Agregacion de operadores en Array

--- basico/Array.js
+++ basico/Array.js
@@ -4,4 +4,11 @@ const Array =["Hola", 5, "Array"];
    
 
 //Anidar un array dentro de otro
-array=["Hola", 5, "Array", [1, 2, 3]];
+Array=["Hola", 5, "Array", [1, 2, 3]];
+
+//Aceder a los datos de un array mediante su indice
+console.log(Array[0]); // "Hola"
+const data = Array[2]; // "Array"
+
+//modificar un array con indices 
+Array[1] =15;
