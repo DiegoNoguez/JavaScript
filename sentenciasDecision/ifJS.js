8b2bdcf53374c68722f41dc9d5907771ad50cf2e
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-09-19 22:11:11 -0600

    Sentencias if y ifElse

--- sentenciasDecision/ifJS.js
+++ sentenciasDecision/ifJS.js
@@ -3,3 +3,23 @@
     es decir un if anidado en JS 
 */
 
+let number = 5;
+
+//Preuba 
+if(number > 0 ){
+    console.log("Valor psositivo distinto de cero: ",number);
+
+}
+//Esto es la sentencia if normal y podemos percaatarno que es similar a java
+
+
+// console.log("El valor es cero o menor a cero.")
+
+
+//if else 
+
+if(number >= 7){
+    console.log("El valor dado es mayor a 7");
+}else{
+    console.log("El valor de ",number, " es menor a 7");
+}
