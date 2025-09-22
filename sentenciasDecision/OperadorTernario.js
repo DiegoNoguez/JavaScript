3db5449fa852c34fdc9142ccd5844922c745c432
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-09-21 21:21:12 -0600

    Operador ternario JS

---
+++ sentenciasDecision/OperadorTernario.js
@@ -0,0 +1,7 @@
+// Uso del operador ternario dentro de JS
+//Es una simplificacion de la sentecia if else
+// Usado para bloque de codigo que solo tengan una linea de codigo ya 
+// que para varias lineas de codigo es necesario usar el bloque if else normañ.
+let num = 50;
+
+(num < 0) ? console.log("Es numero negativo: ", number) : console.log("Es POSITIVO");
