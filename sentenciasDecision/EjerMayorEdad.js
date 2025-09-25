0346f971743cb6febb694f46166f3b66eb3a5061
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-09-24 21:53:16 -0600

    Se ingresa un ejemplo de algoritmo con IfELSE

---
+++ sentenciasDecision/EjerMayorEdad.js
@@ -0,0 +1,13 @@
+//Algoritmo para definir si una persona es mayor o menor mediante
+//el uso de if
+const EDAD_ADULTO=18;
+
+let edad=20;
+
+//Punto de evaluacion de la decision
+
+if(edad >=EDAD_ADULTO){
+    console.log("Es una persona mayor de edad")
+}else{
+    console.log("Es una persona menor de edad")
+}
