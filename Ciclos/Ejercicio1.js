da4802ae87994d83a48590d932d601ed0b67a966
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-08 17:00:12 -0600

    Se ingresa otro ejemplo para for

---
+++ Ciclos/Ejercicio1.js
@@ -0,0 +1,17 @@
+// Impresion de los primeros 10 numeros de 3 en 3 
+// tanto en psoitivo como negativo 
+// Ejemplo de impresion 1 4 7 10
+// Negativo 1 -2 -5 -8
+// Inicio del ejercicio 
+
+let numeroIncrementar = 1;
+
+for(numeroIncrementar; numeroIncrementar <= 10; numeroIncrementar+=3){
+    console.log(numeroIncrementar+"\t");
+}
+
+numeroIncrementar = 1;
+console.log("Impresion de los numeros en formato negativo");
+for(numeroIncrementar; numeroIncrementar >= -10; numeroIncrementar -=3){
+    console.log(numeroIncrementar+ "\t");
+}
