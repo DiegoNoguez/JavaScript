04f520a919570282d7b759712285445be1edf876
Diego Arturo Noguez Lopez <154560222+DiegoNoguez@users.noreply.github.com>
2025-10-12 22:03:21 -0600

    Actualizar EjerMayorEdad.js

--- sentenciasDecision/EjerMayorEdad.js
+++ sentenciasDecision/EjerMayorEdad.js
@@ -1,10 +1,10 @@
 //Algoritmo para definir si una persona es mayor o menor mediante
-//el uso de if
+//el uso de if else en JS
 const EDAD_ADULTO=18;
 
-let edad=20; //Variable a cambiar para visulizar que tanto esta cambiando o se comporta.
+let edad=20; //Variable a cambiar para visulizar que tanto esta cambiando o se comporta nuestro bloque if-else.
 
-//Punto de evaluacion de la decision
+// Punto de evaluacion para determinar edad
 
 if(edad >=EDAD_ADULTO){
     console.log("Es una persona mayor de edad")
