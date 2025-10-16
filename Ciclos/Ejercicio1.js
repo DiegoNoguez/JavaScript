cc8167dc452068060b28455925476a1ac3315450
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-15 20:02:21 -0600

    Se agrega ejercicio Nuevo

--- Ciclos/Ejercicio1.js
+++ Ciclos/Ejercicio1.js
@@ -5,13 +5,14 @@
 // Inicio del ejercicio 
 
 let numeroIncrementar = 1;
-
+console.log("Numeros en incremento");
 for(numeroIncrementar; numeroIncrementar <= 10; numeroIncrementar+=3){
     console.log(numeroIncrementar+"\t");
 }
 
 // EJercio con numeros negativos.
 numeroIncrementar = 1;
+console.log ("Numeros en decremento");
 console.log("Impresion de los numeros en formato negativo");
 for(numeroIncrementar; numeroIncrementar >= -10; numeroIncrementar -=3){
     console.log(numeroIncrementar+ "\t");
---
+++ Ciclos/sumaAcumulativa.js
@@ -0,0 +1,12 @@
+// CEjerccio de suma acumulativa. 
+
+// Suma de los primerso 5 numeros con ciclo for
+
+let n1=1, n2=2;
+
+for(i=1; i<= 4;i++){
+    resultado=n1+ n2
+    console.log("Resultado: ", resultado);
+    n1= resultado;
+    n2++
+}
