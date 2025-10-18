76052e36c7ce1f6609a6f42dc54633e430d96189
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-17 19:52:08 -0600

    Actualizacion y nuevo ejemplo de arrays

---
+++ basico/Arrays.js
@@ -0,0 +1,10 @@
+// Ejemplo Arrays
+let numeroArray= [];
+
+// Modificar valores del array puede ser cualquier tipo de dato.
+numeroArray[0]=13;
+numeroArray[1]=5;
+numeroArray[2]=3;
+numeroArray[3]='Adios';
+
+console.log(numeroArray);
