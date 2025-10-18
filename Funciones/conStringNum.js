c455d3ba244ce6629e161ff3011212527e92ddc4
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-18 17:55:30 -0600

    Conversion

---
+++ Funciones/conStringNum.js
@@ -0,0 +1,19 @@
+// Conversion de un string a JS
+let a = '10', b ='20';
+
+// Al ser concadenas se contatenan.
+console.log(a+b);
+
+// Conversion a numeros para que se sumen
+let suma = parseInt(a) + parseInt(b);
+console.log(suma);
+
+// Conversion de numeros a cadenas.
+let zx= 50, xy= 70, sum;
+
+sum = zx + xy;
+console.log(sum);
+
+// conversion a string
+let concadenar = zx.toString() + xy.toString()
+console.log(concadenar);
