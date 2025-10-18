2c8ae75e736fe840ac5ac421326bd2ef0ca465e3
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-17 22:48:58 -0600

    Se actualiza el archivo de matriz y se agrega carpeta y ejemplo de funciones

---
+++ Funciones/Proced.js
@@ -0,0 +1,9 @@
+// Prcedimiento en JS
+// 1.- Definir el procedimiento 
+function saludar(mensaje){
+    // Cuerpo de funcion
+    console.log(`Mensaje: ${mensaje}`); 
+}
+
+// 2.- Llamar al procedimiento
+saludar('Hola soy una Función');
--- basico/matrices.js
+++ basico/matrices.js
@@ -20,5 +20,24 @@ console.log(`Acceso a un elemento[0][2] = ${matriz[0][2]}`);
 console.log(matriz);
 
 
-// SINTAXIS SIMPLIFICADA PARA DECALRAR UNA MATRIZ 
+// SINTAXIS SIMPLIFICADA PARA DECALRAR UNA MATRIZ  E ITERACION 
 
+let matriz2 = [[50,'Hola',54.5],[87,48,'JS'],[890,45,78,47]]
+
+// Renglones de la matriz 
+console.log(matriz2.length);
+
+// N0 columnas depende del regnglo que se selecciones.
+console.log(matriz2[0].length);
+console.log(matriz2[1].length);  // muestra las columnas
+console.log(matriz2[2].length);
+
+console.log("Impresion de la matriz mediante el uso del for");
+// Lectura de datos con impresion de forma ordenada con for Anidado
+for(let i = 0; i < matriz2.length; i++){
+    // Lectura de columnas
+    for(let j = 0; j< matriz2[i].length; j++){
+        console.log(matriz2[i][j], "\t");
+    }
+    console.log("\n")
+}
