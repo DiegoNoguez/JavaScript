c398bd9d03d5b857afbf723bbff56f7d1843e847
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-20 09:51:35 -0600

    Prog en Objetos

---
+++ ObjetosJS/Objetcs.js
@@ -0,0 +1,16 @@
+// Programacion orientada a objetos  definiendo primero los objetos.
+let x =  10;
+console.log(x.length)
+
+let persona = {
+    nombre: 'alberto',
+    apellido: 'Diaz',
+    gmail: 'adia@gmail.com',
+    edad: 28
+}
+
+console.log(persona.apellido);
+console.log(persona.gmail);
+console.log(persona.nombre);
+
+console.log(persona);
