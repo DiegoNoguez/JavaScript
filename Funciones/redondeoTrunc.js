c83b4e79784b062079f12023a709eef4f5ae8088
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-10-20 09:50:05 -0600

    Nuevos ejemplos de funciones

---
+++ Funciones/redondeoTrunc.js
@@ -0,0 +1,13 @@
+// Operadores de rendodeo y truncado en JS
+let numero = 8.6, rendodeo, truncado;
+
+// redondeo
+// math.round() redondea al valor entero mas cercano
+
+rendodeo= Math.round(numero);
+console.log(rendodeo);
+
+// Truncado
+//  math.trunc elimina la parte flotante 
+truncado = Math.trunc(truncado);
+console.log(truncado);
---
+++ Funciones/valorAbsoluto.js
@@ -0,0 +1,5 @@
+// Valor absoluto en JS
+let numero = -10;
+let valorAbsoluto = Math.abs(numero);
+
+console.log(valorAbsoluto);
